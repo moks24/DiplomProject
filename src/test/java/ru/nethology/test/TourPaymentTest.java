7fc430d699728f24182fcaf8e78a4a3c2ea2b6cf
Moks <gutamax2@mail.ru>
2022-03-18 12:28:50 +0500

    add allure

--- src/test/java/ru/nethology/test/TourPaymentTest.java
+++ src/test/java/ru/nethology/test/TourPaymentTest.java
@@ -1,5 +1,9 @@
 package ru.nethology.test;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.nethology.page.CardDetails;
@@ -10,6 +14,14 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class TourPaymentTest {
 
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
     @BeforeEach
     void setup() {
         open("http://localhost:8080");
